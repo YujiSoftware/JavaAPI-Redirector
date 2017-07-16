490c6ea57c59031c06922592d644b29d2e3d69bb
YujiSoftware <yuji.software+github@gmail.com>
2017-07-16 19:43:48 +0900

    Fiexed does not work in Google Chrome

--- document-jp.js
+++ document-jp.js
@@ -1,4 +1,4 @@
-var location = window.location.href.replace(/https?\:\/\/docs\.oracle\.com\/javase\/jp\/[678]\/(docs\/)?/g, "https://docs.oracle.com/javase/jp/8/docs/");
-if(window.location.href != location){
-  window.location.replace(location);
+var redirectTo = window.location.href.replace(/https?\:\/\/docs\.oracle\.com\/javase\/jp\/[678]\/(docs\/)?/g, "https://docs.oracle.com/javase/jp/8/docs/");
+if(window.location.href != redirectTo){
+  window.location.replace(redirectTo);
 }
