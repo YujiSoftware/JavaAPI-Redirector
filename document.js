71e81779144f99d2de3dde32eeb4e549e41c1aaf
YujiSoftware <yuji.software+github@gmail.com>
2017-07-22 01:16:48 +0900

    Redirect field/constructor/method link

--- document.js
+++ document.js
@@ -1,4 +1,13 @@
 var redirectTo = window.location.href.replace(/https?\:\/\/docs\.oracle\.com\/javase\/[678]/g, "https://docs.oracle.com/javase/8");
 if(window.location.href != redirectTo){
+  redirectTo =
+    redirectTo
+      .replace("#field_summary", "#field.summary")
+      .replace("#constructor_summary", "#constructor.summary")
+      .replace("#method_summary", "#method.summary")
+      .replace("#field_detail", "#field.detail")
+      .replace("#constructor_detail", "#constructor.detail")
+      .replace("#method_detail", "#method.detail");
+
   window.location.replace(redirectTo);
 }
