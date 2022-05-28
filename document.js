ff88b857244afb5973636a40493d4b4b92667fc8
YujiSoftware <yuji.software+github@gmail.com>
2022-05-28 21:17:39 +0900

    Support Java 18

--- document.js
+++ document.js
@@ -33,6 +33,7 @@ const paths = [
   { version: 15, language: "en", hasModule: true, prefix: "/en/java/javase/15/docs/api/" },
   { version: 16, language: "en", hasModule: true, prefix: "/en/java/javase/16/docs/api/" },
   { version: 17, language: "en", hasModule: true, prefix: "/en/java/javase/17/docs/api/" },
+  { version: 18, language: "en", hasModule: true, prefix: "/en/java/javase/18/docs/api/" },
   // Japanese
   { version: 6, language: "jp", hasModule: false, prefix: "/javase/jp/6/api/" },
   { version: 7, language: "jp", hasModule: false, prefix: "/javase/jp/7/api/" },
@@ -47,6 +48,7 @@ const paths = [
   { version: 15, language: "jp", hasModule: true, prefix: "/javase/jp/15/docs/api/" },
   { version: 16, language: "jp", hasModule: true, prefix: "/javase/jp/16/docs/api/" },
   { version: 17, language: "jp", hasModule: true, prefix: "/javase/jp/17/docs/api/" },
+  { version: 18, language: "jp", hasModule: true, prefix: "/javase/jp/18/docs/api/" },
 ];
 
 let javadoc = {
@@ -84,9 +86,9 @@ if(javadoc.found){
     let redirectTo = items.redirectTo;
     if(redirectTo === "latest"){
       if (javadoc.language === "jp") {
-        redirectTo = 17;
+        redirectTo = 18;
       } else {
-        redirectTo = 17; 
+        redirectTo = 18; 
       }
     }
 
