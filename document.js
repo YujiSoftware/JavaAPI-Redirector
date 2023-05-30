e41ec463a5448f2c66887aa6f5e5d3ecc35303ed
YujiSoftware <yuji.software+github@gmail.com>
2023-05-31 01:02:29 +0900

    Fix

--- document.js
+++ document.js
@@ -34,7 +34,7 @@ const paths = [
   { version: 16, language: "en", hasModule: true, prefix: "/en/java/javase/16/docs/api/" },
   { version: 17, language: "en", hasModule: true, prefix: "/en/java/javase/17/docs/api/" },
   { version: 18, language: "en", hasModule: true, prefix: "/en/java/javase/18/docs/api/" },
-  { version: 18, language: "en", hasModule: true, prefix: "/en/java/javase/19/docs/api/" },
+  { version: 19, language: "en", hasModule: true, prefix: "/en/java/javase/19/docs/api/" },
   // Japanese
   { version: 6, language: "jp", hasModule: false, prefix: "/javase/jp/6/api/" },
   { version: 7, language: "jp", hasModule: false, prefix: "/javase/jp/7/api/" },
@@ -50,7 +50,7 @@ const paths = [
   { version: 16, language: "jp", hasModule: true, prefix: "/javase/jp/16/docs/api/" },
   { version: 17, language: "jp", hasModule: true, prefix: "/javase/jp/17/docs/api/" },
   { version: 18, language: "jp", hasModule: true, prefix: "/javase/jp/18/docs/api/" },
-  { version: 18, language: "jp", hasModule: true, prefix: "/javase/jp/19/docs/api/" },
+  { version: 19, language: "jp", hasModule: true, prefix: "/javase/jp/19/docs/api/" },
 ];
 
 let javadoc = {
